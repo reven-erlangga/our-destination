1cb5cee36a50d7562e36f4d57c254c59b5fc4b3a
reven-erlangga <reven.erlangga@gmail.com>
2022-12-29 21:31:33 +0700

    add: adding input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import "./NewPlace.css";
+
+import Input from "../../shared/components/FormElements/Input";
+
 const NewPlace = () => {
-  return <h2>New Place</h2>;
+  return (
+    <form className="place-form">
+      <Input element="input" type="text" label="Title" />
+    </form>
+  );
 };
 
 export default NewPlace;
