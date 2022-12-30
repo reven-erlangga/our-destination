8c07cbbbcf0200cb126cc7caacc83c927ad937e4
reven-erlangga <reven.erlangga@gmail.com>
2022-12-30 16:29:18 +0700

    add: adding auth context for app wide state management

--- src/App.js
+++ src/App.js
@@ -13,23 +13,39 @@ import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./user/pages/Auth";
 import { Redirect } from "react-router-dom";
+import { AuthContext } from "./shared/context/auth-context";
+import { useCallback, useState } from "react";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Routes>
-          <Route path="*" element={<Navigate to="/" replace />} />
-          <Route path="/:userId/places" element={<UserPlaces />} exact />
-          <Route path="/" element={<Users />} />
-          <Route path="/places/create" element={<NewPlace />} exact />
-          <Route path="/places/:placeId" element={<UpdatePlace />} exact />
-          <Route path="/auth" element={<Auth />} exact />
-          <Redirect tp="/" />
-        </Routes>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          <Routes>
+            <Route path="*" element={<Navigate to="/" replace />} />
+            <Route path="/:userId/places" element={<UserPlaces />} exact />
+            <Route path="/" element={<Users />} />
+            <Route path="/places/create" element={<NewPlace />} exact />
+            <Route path="/places/:placeId" element={<UpdatePlace />} exact />
+            <Route path="/auth" element={<Auth />} exact />
+            <Redirect tp="/" />
+          </Routes>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
