fb8e329e946d4a2bbd849e2d2797bde491e7b961
reven-erlangga <reven.erlangga@gmail.com>
2022-12-30 23:17:34 +0700

    add: adding handling error in app.js

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,18 @@ const port = 5000;
 
 app.use("/api/places", placesRoutes);
 
+app.use((err, req, res, next) => {
+  if (res.headerSent) {
+    return next(err);
+  }
+
+  res.status(err.code || 500);
+  res.json({
+    data: null,
+    message: err.message || "An unknown error occurred",
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- backend/routes/places-route.js
+++ backend/routes/places-route.js
@@ -2,15 +2,67 @@ const express = require("express");
 
 const router = express.Router();
 
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "P2",
+    description: "p1",
+    imageUrl:
+      "https://images.unsplash.com/photo-1583022846753-83a4eba54ac1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    address: "p1",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+];
+
 router.get("/", (req, res, next) => {
   res.json({
     message: "Its work",
   });
 });
-router.get("/:id", (req, res, next) => {
-  const placeId = req.params.id;
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    const err = new Error("Could not find a place for the provide id.");
+    err.code = 404;
+
+    return next(err);
+  }
+
   res.json({
-    message: placeId,
+    data: {
+      place: place,
+    },
+  });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    const err = new Error("Could not find a place for the provide user id.");
+    err.code = 404;
+
+    return next(err);
+  }
+
+  res.json({
+    data: {
+      place: place,
+    },
   });
 });
 
