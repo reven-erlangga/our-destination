c8174ccfa97f78dd3ebb8e8ef155843271c96d6f
reven-erlangga <reven.erlangga@gmail.com>
2022-12-30 16:32:14 +0700

    fix: listening to context change in navlink

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "./shared/context/auth-context";
 
 import "./NavLinks.css";
 
 const NavLinks = (props) => {
+  const auth = useContext(AuthContext);
+
   return (
     <ul className="nav-links">
       <li>
@@ -11,15 +14,21 @@ const NavLinks = (props) => {
           All Users
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/u1/places">My Places</NavLink>
-      </li>
-      <li>
-        <NavLink to="/places/create">Add Places</NavLink>
-      </li>
-      <li>
-        <NavLink to="/auth">Auth</NavLink>
-      </li>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/u1/places">My Places</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/places/create">Add Places</NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">Auth</NavLink>
+        </li>
+      )}
     </ul>
   );
 };
