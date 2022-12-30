1c9139a9f831bceb6c1d28b04079c3256e3941eb
reven-erlangga <reven.erlangga@gmail.com>
2022-12-30 07:49:52 +0700

    add: adding validation method in new place file

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./NewPlace.css";
 
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/utils/validators";
 
 const NewPlace = () => {
   return (
@@ -11,7 +12,7 @@ const NewPlace = () => {
         element="input"
         type="text"
         label="Title"
-        validator={[]}
+        validator={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
       />
     </form>
