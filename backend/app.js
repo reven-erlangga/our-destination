24dae7ce636bd276ed9fee5bdcc0a93ba4220663
reven-erlangga <reven.erlangga@gmail.com>
2023-01-03 20:22:16 +0700

    add: handling error for unsupported routes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-route");
+const HttpError = require("./models/http-error");
 
 const app = express();
 const port = 5000;
@@ -9,6 +10,12 @@ const port = 5000;
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes);
+
+app.use((req, res, next) => {
+  const err = new HttpError("Could not find this route", 404);
+
+  throw err;
+});
 
 app.use((err, req, res, next) => {
   if (res.headerSent) {
