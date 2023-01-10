fdfd8ecbf8151d3a71108a44990e0cb2392c5f89
reven-erlangga <reven.erlangga@gmail.com>
2023-01-10 20:20:36 +0700

    add: check deleted place in place controller

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -108,6 +108,10 @@ const updatePlaceById = (req, res, next) => {
 const deletePlaceById = (req, res, next) => {
   const placeId = req.params.pid;
 
+  if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
+    throw new HttpError("Could not find a place for that id", 404);
+  }
+
   DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
 
   res.status(200).json({
