01d2d2892062dd4f973f1246d9c0182100831055
reven-erlangga <reven.erlangga@gmail.com>
2023-01-12 21:08:30 +0700

    add: sending a post request to the backend (frontend)

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -30,9 +30,27 @@ const Auth = () => {
     false
   );
 
-  const authSubmitHandler = (event) => {
+  const authSubmitHandler = async (event) => {
     event.preventDefault();
-    console.log(formState.inputs);
+
+    if (isLoginMode) {
+    } else {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+      } catch (error) {}
+    }
     auth.login();
   };
 
