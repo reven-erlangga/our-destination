e57419aef2c96e26500cad68f55eb3731961bd1c
reven-erlangga <reven.erlangga@gmail.com>
2023-01-21 16:24:46 +0700

    add: manage token expiration date

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,14 +19,19 @@ const App = () => {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(false);
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
+
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+
     localStorage.setItem(
       "userData",
       JSON.stringify({
         userId: uid,
         token: token,
+        expiration: tokenExpirationDate.toISOString(),
       })
     );
   }, []);
@@ -40,8 +45,16 @@ const App = () => {
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem("userData"));
 
-    if (storedData && storedData.token) {
-      login(storedData.userId, storedData.token);
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
     }
   }, [login]);
 
