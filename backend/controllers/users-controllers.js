03f3066096f469887ff0fbbc71cad8e483024fe2
reven-erlangga <reven.erlangga@gmail.com>
2023-01-21 13:53:53 +0700

    add: hasing check in login method (user controllers)

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -112,13 +112,34 @@ const login = async (req, res, next) => {
     return next(err);
   }
 
-  if (!existingUser || existingUser.password != password) {
+  if (!existingUser) {
     const err = new HttpError(
       "Invalid credentials, could not log you in.",
       401
     );
 
     return next(err);
+  }
+
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not log you in, please check your credentials",
+      500
+    );
+
+    return next(error);
+  }
+
+  if (!isValidPassword) {
+    const error = new HttpError(
+      "Invalid credential, could not log you in",
+      500
+    );
+
+    return next(error);
   }
 
   res.json({
