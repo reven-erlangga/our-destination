5e419598f16efe766af276298ae5f06923d270fe
reven-erlangga <reven.erlangga@gmail.com>
2023-01-21 19:12:10 +0700

    fix: some code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,15 +6,17 @@ import {
   Routes,
 } from "react-router-dom";
 
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import Auth from "./user/pages/Auth";
 import { AuthContext } from "./shared/context/auth-context";
-import React from "react";
+import React, { Suspense } from "react";
 import { useAuth } from "./shared/hooks/auth-hook";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner";
+
+const Users = React.lazy(() => import("./user/pages/Users"));
+const NewPlace = React.lazy(() => import("./user/pages/NewPlace"));
+const UserPlaces = React.lazy(() => import("./user/pages/UserPlaces"));
+const UpdatePlace = React.lazy(() => import("./user/pages/UpdatePlace"));
+const Auth = React.lazy(() => import("./user/pages/Auth"));
 
 const App = () => {
   const { token, login, logout, userId } = useAuth();
@@ -53,7 +55,17 @@ const App = () => {
     >
       <Router>
         <MainNavigation />
-        <main>{routes}</main>
+        <main>
+          <Suspense
+            fallback={
+              <div className="center">
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            {routes}
+          </Suspense>
+        </main>
       </Router>
     </AuthContext.Provider>
   );
